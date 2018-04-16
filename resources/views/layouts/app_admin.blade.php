4e5128a485ee4a8b1af91ce24cbf1d4ffd96f454
Kiril Kirkov <kiro@inv.bg>
2018-04-16 17:46:16 +0300

    Fix meta tags xhtml
    meta tag user scalable fix

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{__('admin_pages.admin_panel_title').$page_title_lang}}</title>   
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" /> 
         <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet" />
         <link href="{{ asset('css/adminCustom.css') }}" rel="stylesheet" /> 
-        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+        <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css' />
         <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
     </head>
     <body>
--- resources/views/layouts/app_public.blade.php
+++ resources/views/layouts/app_public.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         <meta name="csrf-token" content="{{ csrf_token() }}"/>
         <meta name="description" content="{{ $head_description }}"/>
         <meta property="og:title" content="{{ $head_title }}" />
